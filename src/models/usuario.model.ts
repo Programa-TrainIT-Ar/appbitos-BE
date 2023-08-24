99cb5d133a8d0eb75e7e61a93dce049b3489e445
Pablo Abal Diez <pabloabaldiez@gmail.com>
2023-08-24 19:24:35 -0300

    modelo y crud sin delete

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha_nacimiento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  genero: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
